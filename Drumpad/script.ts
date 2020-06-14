9f91f2667c238ca757587845777ad607a527e04c
AlessCh <64133065+AlessCh@users.noreply.github.com>
2020-06-14 16:08:28 +0200

    upload

--- Drumpad/script.ts
+++ Drumpad/script.ts
@@ -2,9 +2,6 @@
 
 //aufgabe 7.1: drumpad//
 
-// tslint:disable-next-line: typedef
-window.addEventListener("load", function () {
-
 var padsound1: HTMLAudioElement = new Audio("A.mp3");
 var padsound2: HTMLAudioElement = new Audio("C.mp3");
 var padsound3: HTMLAudioElement = new Audio("F.mp3");
@@ -15,14 +12,23 @@ var padsound7: HTMLAudioElement = new Audio("laugh-1.mp3");
 var padsound8: HTMLAudioElement = new Audio("laugh-2.mp3");
 var padsound9: HTMLAudioElement = new Audio("snare.mp3");
 
-// tslint:disable-next-line: typedef
 var drumpad = [padsound1, padsound2, padsound3, padsound4, padsound5, padsound6, padsound7, padsound8, padsound9];
 
-// tslint:disable-next-line: typedef
 function playSample(sound: HTMLAudioElement) {
 sound.play();
 }
 
+//aufgabe 7.2: drum machine//
+function drummachine() {
+    setInterval(
+        function () {
+            playSample(drumpad[5]);
+            playSample(drumpad[8]);
+            playSample(drumpad[4]);
+    }, 200;
+}
+
+window.addEventListener("load", function () {
 document.getElementById("pad1").addEventListener("click", function () { playSample(drumpad[0]); });
 document.getElementById("pad2").addEventListener("click", function () { playSample(drumpad[1]); });
 document.getElementById("pad3").addEventListener("click", function () { playSample(drumpad[2]); });
@@ -32,22 +38,5 @@ document.getElementById("pad6").addEventListener("click", function () { playSamp
 document.getElementById("pad7").addEventListener("click", function () { playSample(drumpad[6]); });
 document.getElementById("pad8").addEventListener("click", function () { playSample(drumpad[7]); });
 document.getElementById("pad9").addEventListener("click", function () { playSample(drumpad[8]); });
-
-
-//aufgabe 7.2: drum machine//
-
-function PBM() {
-
-    // tslint:disable-next-line: typedef
-    setInterval(function() {
-    padsound1.play();
-    padsound2.play();
-    padsound3.play();
-},              500);
-
-
-    document.getElementById("playbutton").addEventListener("click",  function () { PBM(); });
-
-}
-
+document.getElementById("playbutton").addEventListener("click", drummachine);
 });
