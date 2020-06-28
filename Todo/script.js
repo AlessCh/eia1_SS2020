dab4bc456ae36bd4439348a6b7fd62331539643c
AlessCh <64133065+AlessCh@users.noreply.github.com>
2020-06-28 14:34:12 +0200

    upload

---
+++ Todo/script.js
@@ -0,0 +1,86 @@
+"use strict";
+//Code Tutorial from a Youtube video: https://www.youtube.com/watch?v=b8sUhU_eq3g //
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
+// variable
+let LIST, id;
+// get item
+let data = localStorage.getItem("TODO");
+// is data empty?
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}
+else {
+    LIST = [];
+    id = 0;
+}
+// load items
+function loadList(array) {
+    array.forEach(function (item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+//ADD TO DO
+function addToDo(toDo, id, done, trash) {
+    if (trash) {
+        return;
+    }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+//user clicks enter
+document.addEventListener("keyup", function (even) {
+    if (event.keyCode == 13) {
+        const toDo = input.value;
+        if (toDo) {
+            addToDo(toDo, id, false, false);
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
+            id++;
+            document.querySelector("#counter").innerHTML = ("Total: " + id);
+        }
+        input.value = "";
+    }
+});
+//check/uncheck
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+//delete
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+    id--;
+    document.querySelector("#counter").innerHTML = ("Total: " + id);
+}
+list.addEventListener("click", function (event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+    if (elementJob == "complete") {
+        completeToDo(element);
+    }
+    else if (elementJob == "delete") {
+        removeToDo(element);
+    }
+});
+//# sourceMappingURL=script.js.map
