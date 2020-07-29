7e801dae683488fce1da6c84be7bd06f07168700
AlessCh <64133065+AlessCh@users.noreply.github.com>
2020-07-29 18:26:58 +0200

    upload

--- Endaufgabe/script.js
+++ Endaufgabe/script.js
@@ -1,17 +1,14 @@
 "use strict";
-var button1 = new Audio("sound_boing.mp3");
-var button2 = new Audio("sound_kids_booo.mp3");
-var button3 = new Audio("sound_whistle.mp3");
-var button4 = new Audio("sound_yeah.mp3");
-var button5 = new Audio("sound_squeaking.mp3");
-function playSample(sound) {
-    sound.play();
-}
 window.addEventListener("load", function () {
-    document.getElementById("button1").addEventListener("click", function () { playSample(drumpad[0]); });
-    document.getElementById("button2").addEventListener("click", function () { playSample(drumpad[1]); });
-    document.getElementById("button3").addEventListener("click", function () { playSample(drumpad[2]); });
-    document.getElementById("button4").addEventListener("click", function () { playSample(drumpad[3]); });
-    document.getElementById("button5").addEventListener("click", function () { playSample(drumpad[4]); });
+    document.getElementById("pad1").addEventListener("click", function () { playSample("sound_boing.mp3"); });
+    document.getElementById("pad2").addEventListener("click", function () { playSample("sound_laugh.mp3"); });
+    document.getElementById("pad3").addEventListener("click", function () { playSample("sound_squeaking.mp3"); });
+    document.getElementById("pad4").addEventListener("click", function () { playSample("sound_whistle.mp3"); });
+    document.getElementById("pad5").addEventListener("click", function () { playSample("sound_yeah.mp3"); });
+    //8.6 play sample + new audio//
+    function playSample(mp3) {
+        const sound = new Audio(mp3);
+        sound.play();
+    }
 });
 //# sourceMappingURL=script.js.map
--- Endaufgabe/script.ts
+++ Endaufgabe/script.ts
@@ -1,18 +1,17 @@
-var button1: HTMLAudioElement = new Audio("sound_boing.mp3");
-var button2: HTMLAudioElement = new Audio("sound_kids_booo.mp3");
-var button3: HTMLAudioElement = new Audio("sound_whistle.mp3");
-var button4: HTMLAudioElement = new Audio("sound_yeah.mp3");
-var button5: HTMLAudioElement = new Audio("sound_squeaking.mp3");
 
-function playSample(sound: HTMLAudioElement) {
-    sound.play();
-    }
-    
 window.addEventListener("load", function () {
-    document.getElementById("button1").addEventListener("click", function () { playSample(drumpad[0]); });
-    document.getElementById("button2").addEventListener("click", function () { playSample(drumpad[1]); });
-    document.getElementById("button3").addEventListener("click", function () { playSample(drumpad[2]); });
-    document.getElementById("button4").addEventListener("click", function () { playSample(drumpad[3]); });
-    document.getElementById("button5").addEventListener("click", function () { playSample(drumpad[4]); });
 
+document.getElementById("pad1").addEventListener("click", function () {playSample("sound_boing.mp3"); });
+document.getElementById("pad2").addEventListener("click", function () {playSample("sound_laugh.mp3"); });
+document.getElementById("pad3").addEventListener("click", function () {playSample("sound_squeaking.mp3"); });
+document.getElementById("pad4").addEventListener("click", function () {playSample("sound_whistle.mp3"); });   
+document.getElementById("pad5").addEventListener("click", function () {playSample("sound_yeah.mp3"); });
+
+
+//8.6 play sample + new audio//
+
+function playSample(mp3: string): void {
+    const sound: HTMLAudioElement = new Audio(mp3);
+    sound.play();
+}
 });
